aeca4597ea946e38959eec50d9e4e8d67c091858
ccald005 <ccald005@fiu.edu>
2024-01-28 15:56:23 -0500

    tester

--- assignment2.py
+++ assignment2.py
@@ -4,9 +4,8 @@ from datetime import datetime
 
 @staticmethod
 def checkGoodString(string):
-    count = sum(c.isdigit() for c in string)
     if (len(string) < 9) or (string.capitalize() == string)\
-    or (count != 1):
+    or (sum(c.isdigit() for c in string) != 1):
         return False
     return True
 
@@ -26,38 +25,35 @@ class Assignment2:
         self.year = year
 
     def tellAge(self, current_year):
-        print("Your age is %d" % (current_year - self.year))
-        # sys.stdout.write("Hello %s\n" (self.year))
+        sys.stdout.write("Your age is %d\n" % (current_year - self.year))
 
     def listAnneversaries(self):
-        list = []
-        x = self.year
-        n = 0
-        current = 2021
-        while (current - x) >= 10:
-            n += 10
-            list.append(n)
-            x+=10
-        return list
+        return list(range(10, (2022-self.year), 10))
     
     def modifyYear(self, n):
         return str(self.year)[:2]*n + str(self.year*n)[::2]
 
 
 a = Assignment2(2000)
-a.tellAge(datetime.now().year)
+a.tellAge((datetime.now().year)-2)
 rv = a.listAnneversaries()
 print(rv)
+
 rv = checkGoodString("f1obar0more")
 print(rv)
 rv = checkGoodString("foobar0more")
 print(rv)
 
-rv = a.modifyYear(5)
-print(rv)
+# rv = a.modifyYear(5)
+# print(rv)
 
-rv = connectTcp("www.google.com", 80)
-if rv:
-    print("success")
-else:
-    print("fail")
+# rv = connectTcp("www.google.com", 80)
+# if rv:
+#     print("success")
+# else:
+#     print("fail")
+
+x  = 22//10
+
+
+print("%d" % x)
